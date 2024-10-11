stefansundin/metabase

work on dedicated dashboard question page

frontend/src/metabase/query_builder/components/view/ViewHeader/components/QuestionActions/QuestionActions.tsx
@@ -92,6 +92,10 @@ export const QuestionActions = ({
     : undefined;
 
   const isQuestion = question.type() === "question";
+  const isDashboardQuestion =
+    isQuestion && typeof question.dashboardId() !== undefined;
+  const isStandaloneQuestion =
+    isQuestion && question.dashboardId() === undefined;
   const isModel = question.type() === "model";
   const isMetric = question.type() === "metric";
   const isModelOrMetric = isModel || isMetric;
@@ -186,7 +190,7 @@ export const QuestionActions = ({
     });
   }
 
-  if (isQuestion || isMetric) {
+  if (isStandaloneQuestion || isMetric) {
     extraButtons.push({
       title: t`Add to dashboard`,
       icon: "add_to_dash",
@@ -195,7 +199,7 @@ export const QuestionActions = ({
     });
   }
 
-  if (hasCollectionPermissions) {
+  if (isStandaloneQuestion && hasCollectionPermissions) {
     extraButtons.push({
       title: t`Move`,
       icon: "move",
@@ -214,7 +218,7 @@ export const QuestionActions = ({
   }
 
   if (hasCollectionPermissions) {
-    if (isQuestion) {
+    if (isStandaloneQuestion) {
       extraButtons.push({
         title: t`Turn into a model`,
         icon: "model",
@@ -233,7 +237,7 @@ export const QuestionActions = ({
 
   extraButtons.push(...PLUGIN_QUERY_BUILDER_HEADER.extraButtons(question));
 
-  if (hasCollectionPermissions) {
+  if (!isDashboardQuestion && hasCollectionPermissions) {
     extraButtons.push({
       title: t`Move to trash`,
       icon: "trash",
@@ -337,10 +341,18 @@ export const QuestionActions = ({
       )}
       {extraButtons.length > 0 && !question.isArchived() && (
         <EntityMenu
-          triggerAriaLabel={t`Move, trash, and more...`}
+          triggerAriaLabel={
+            isDashboardQuestion
+              ? t`Verify, duplicate, and more...`
+              : t`Move, trash, and more...`
+          }
           items={extraButtons}
           triggerIcon="ellipsis"
-          tooltip={t`Move, trash, and more...`}
+          tooltip={
+            isDashboardQuestion
+              ? t`Verify, duplicate, and more...`
+              : t`Move, trash, and more...`
+          }
         />
       )}
     </>

srikanths074/metabase15028

frontend/src/metabase/query_builder/components/view/ViewHeader/components/QuestionActions/QuestionActions.tsx
@@ -1,6 +1,7 @@
 import type { ChangeEvent } from "react";
 import { useCallback, useRef, useState } from "react";
 import { t } from "ttag";
+import _ from "underscore";
 
 import { useSetting } from "metabase/common/hooks";
 import EntityMenu from "metabase/components/EntityMenu";
@@ -91,6 +92,9 @@ export const QuestionActions = ({
     : undefined;
 
   const isQuestion = question.type() === "question";
+  const isDashboardQuestion = isQuestion && _.isNumber(question.dashboardId());
+  const isStandaloneQuestion =
+    isQuestion && question.dashboardId() === undefined;
   const isModel = question.type() === "model";
   const isMetric = question.type() === "metric";
   const isModelOrMetric = isModel || isMetric;
@@ -125,7 +129,7 @@ export const QuestionActions = ({
 
   const extraButtons = [];
 
-  if (isQuestion || isMetric) {
+  if (isStandaloneQuestion || isMetric) {
     extraButtons.push({
       title: t`Add to dashboard`,
       icon: "add_to_dash",
@@ -177,7 +181,7 @@ export const QuestionActions = ({
   }
 
   if (hasCollectionPermissions) {
-    if (isQuestion) {
+    if (isStandaloneQuestion) {
       extraButtons.push({
         title: t`Turn into a model`,
         icon: "model",
@@ -335,10 +339,18 @@ export const QuestionActions = ({
       )}
       {extraButtons.length > 0 && !question.isArchived() && (
         <EntityMenu
-          triggerAriaLabel={t`Move, trash, and more...`}
+          triggerAriaLabel={
+            isDashboardQuestion
+              ? t`Move, duplicate, and more...`
+              : t`Move, trash, and more...`
+          }
           items={extraButtons}
           triggerIcon="ellipsis"
-          tooltip={t`Move, trash, and more...`}
+          tooltip={
+            isDashboardQuestion
+              ? t`Move, duplicate, and more...`
+              : t`Move, trash, and more...`
+          }
         />
       )}
     </>